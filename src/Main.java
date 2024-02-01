419884cba3a0ad59011b207602854faeadd76718
Xaliq Quluzade <xaliqquluzade10@gmail.com>
2024-02-01 15:10:45 +0400

    exercise 2 done

---
+++ src/Main.java
@@ -0,0 +1,233 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        printCharacterIndex();
+        replaceEachString();
+        secondMostFrequentCharacter();
+        removeDublicate();
+        divideString();
+        reverseSentence();
+        sumDigitsInString();
+        ascendigOrderByLength();
+        descendigOrderByLength();
+        minimumAndMaximumValue();
+        countNumberInArray();
+    }
+
+    public static void printCharacterIndex() {
+        char startChar = 'a';
+        char endChar = 'z';
+
+        for (char ch = startChar; ch <= endChar; ch++) {
+            System.out.println("index: " + (int) ch + "\tchar: " + ch);
+        }
+    }
+
+    public static void replaceEachString() {
+        String s = "salam sagol cox sagol salam sagol";
+        System.out.println(s.replaceAll("salam", "sagol"));
+
+    }
+
+    public static void secondMostFrequentCharacter() {
+        String s = "successes";
+        String s2 = "";
+        int[] counts = null;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!(s2.contains(Character.toString(s.charAt(i))))) {
+                s2 += s.charAt(i);
+            }
+        }
+
+        counts = new int[s2.length()];
+        for (int i = 0; i < s2.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < s.length(); j++) {
+                if (s2.charAt(i) == s.charAt(j)) {
+                    count++;
+                }
+            }
+            counts[i] = count;
+        }
+
+        Arrays.sort(counts);
+
+        for (int i = 0; i < s2.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < s.length(); j++) {
+                if (s2.charAt(i) == s.charAt(j)) {
+                    count++;
+                }
+            }
+            if (counts[counts.length - 2] == count) {
+                System.out.println(s2.charAt(i));
+            }
+        }
+    }
+
+    public static void removeDublicate() {
+        String s = "w3resource";
+        String s2 = "";
+        int[] counts = null;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!(s2.contains(Character.toString(s.charAt(i))))) {
+                s2 += s.charAt(i);
+            }
+        }
+
+        System.out.println(s2);
+    }
+
+    public static void divideString() {
+        String s = "abcdefghijklmnopqrstuvwxy";
+        int n = 5;
+        int charCount = s.length() / n;
+        String part = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            part += s.charAt(i);
+            if (part.length() == s.length() / 5) {
+                System.out.println(part);
+                part = "";
+            }
+        }
+    }
+
+    public static void reverseSentence() {
+        String s = "Reverse words in a given string";
+        String[] words = s.split(" ");
+        String[] result = new String[words.length];
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            result[result.length - 1 - i] = words[i];
+        }
+
+        System.out.println(String.join(" ", result));
+    }
+
+    public static void sumDigitsInString() {
+        String s = "ab5c2d4ef12s";
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                sum += Character.getNumericValue(c);
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    public static void ascendigOrderByLength() {
+        String[] colors = {"Green", "White", "Black", "Pink", "Orange", "Blue", "Champagne", "Indigo", "Ivory"};
+        String[] result = new String[colors.length];
+        int resultIndex = 0;
+        int maxLength = 0;
+        boolean isEmpty = false;
+
+        while (!isEmpty) {
+            for (int i = 0; i < colors.length; i++) {
+                if (colors[i].isEmpty()) {
+                    continue;
+                }
+                if (colors[i].length() > maxLength) {
+                    maxLength = colors[i].length();
+                }
+            }
+
+            for (int i = 0; i < colors.length; i++) {
+                if (colors[i].length() == maxLength) {
+                    result[resultIndex++] = colors[i];
+                    colors[i] = "";
+                }
+            }
+
+            maxLength = 0;
+
+            isEmpty = true;
+            for (int i = 0; i < colors.length; i++) {
+                if (!colors[i].isEmpty()) {
+                    isEmpty = false;
+                }
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+
+    public static void descendigOrderByLength() {
+        String[] colors = {"Green", "White", "Black", "Pink", "Orange", "Blue", "Champagne", "Indigo", "Ivory"};
+        String[] result = new String[colors.length];
+        int resultIndex = 0;
+        int minLength = Integer.MAX_VALUE;
+        boolean isEmpty = false;
+
+        while (!isEmpty) {
+            for (int i = 0; i < colors.length; i++) {
+                if (colors[i].isEmpty()) {
+                    continue;
+                }
+                if (colors[i].length() < minLength) {
+                    minLength = colors[i].length();
+                }
+            }
+
+            for (int i = 0; i < colors.length; i++) {
+                if (colors[i].length() == minLength) {
+                    result[resultIndex++] = colors[i];
+                    colors[i] = "";
+                }
+            }
+
+            minLength = Integer.MAX_VALUE;
+
+            isEmpty = true;
+            for (int i = 0; i < colors.length; i++) {
+                if (!colors[i].isEmpty()) {
+                    isEmpty = false;
+                }
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+
+    public static void minimumAndMaximumValue() {
+        int[] numbers = {123, 2, 435};
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    public static void countNumberInArray() {
+        int n = 7;
+        int[] numbers = {4, 5, 6, 7, 3, 5, 7, 4, 6, 7, 3, 4, 6, 4,};
+        int count = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == n) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
